6686757f3b60e0cd737259ca98979065f07068c7
VaishnaviBoobesh <vaishnaviboobesh@gmail.com>
2025-10-27 22:03:18 +0100

    Added codewars numbers without fives count kata

---
+++ CodewarsKata/NumberWithoutFives.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsKata;
+
+public class NumberWithoutFives
+{
+	private long count;
+
+	public long Count(long startIndex, long end)
+	{
+		for (var start = startIndex; start <= end; start++)
+			if (!HasFives(start))
+				count++;
+		return count;
+	}
+
+	private static bool HasFives(long currentNumber) => currentNumber.ToString().Contains('5');
+}
---
+++ CodewarsKataTest/NumbersWithoutFives.Tests.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using CodewarsKata;
+using FluentAssertions;
+
+namespace CodewarsKataTest;
+
+public class NumbersWithoutFivesTest
+{
+	[Theory]
+	[InlineData(4, 17, 12)]
+	[InlineData(4, 18, 13)]
+	[InlineData(-5, 5, 9)]
+	public void CountNumbersWithoutFives(long start, long end, long expected) =>
+	new NumberWithoutFives().Count(start, end).Should().Be(expected);
+
+	[Fact]
+	public void CountWithSameStartAndEnd_ShouldReturnOne() =>
+		new NumberWithoutFives().Count(0, 0).Should().Be(1);
+}
